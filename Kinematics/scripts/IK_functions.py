1c68215f1c11b2158dab12b8730349358bd0f57f
Matay Mayrany <mayrany@kth.se>
2019-09-10 20:47:49 +0200

    improvements to part 1 of assignment, basically works now

--- Kinematics/scripts/IK_functions.py
+++ Kinematics/scripts/IK_functions.py
@@ -12,12 +12,21 @@ def scara_IK(point):
     z = point[2]
     q = [0.0, 0.0, 0.0]
 
+    arm1 = 0.3
+    arm2 = 0.35
     r = math.sqrt(y**2 + (x-0.07)**2)
 
+    temp_angle_1 = math.atan(y/(x-0.07))
+    temp_angle_2 = math.acos(((arm1**2) + (arm2**2) - (r**2))/(2*arm1*arm2))
+
     q3 = z
-    q2 = math.acos((r**2 - 0.0325)/(0.6*r))
-    q1 = math.atan(y/(x-0.07)) - q2
+    q2 = math.pi - temp_angle_2
     
+    num = y*(arm2*math.cos(q2)+arm1) - x*(arm2*math.sin(q2))
+    dnum = (x-0.07)*(arm2*math.cos(q2)+arm1) + y*(arm2*math.sin(q2)) 
+    q1 = math.atan2(num,dnum)
+    #q1 = math.asin(y/arm1)
+    #q1 = q2 - temp_angle_1
     q = [q1, q2, q3]
 
     """
