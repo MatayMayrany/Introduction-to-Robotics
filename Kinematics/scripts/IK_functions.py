e5578faa690ee2b2ef69213c8aea6c759dc46ec2
Matay Mayrany <mmayrany@invidi.se>
2019-09-12 21:25:40 +0200

    latest and last attempt

--- Kinematics/scripts/IK_functions.py
+++ Kinematics/scripts/IK_functions.py
@@ -16,22 +16,34 @@ def scara_IK(point):
     arm2 = 0.35
     r = math.sqrt(y**2 + (x-0.07)**2)
 
-    temp_angle_1 = math.atan(y/(x-0.07))
-    temp_angle_2 = math.acos(((arm1**2) + (arm2**2) - (r**2))/(2*arm1*arm2))
+    temp_angle_1 = math.acos(((arm1**2) + (arm2**2) - (r**2))/(2*arm1*arm2))
 
     q3 = z
-    q2 = math.pi - temp_angle_2
-    
-    num = y*(arm2*math.cos(q2)+arm1) - x*(arm2*math.sin(q2))
-    dnum = (x-0.07)*(arm2*math.cos(q2)+arm1) + y*(arm2*math.sin(q2)) 
-    q1 = math.atan2(num,dnum)
-    #q1 = math.asin(y/arm1)
-    #q1 = q2 - temp_angle_1
-    q = [q1, q2, q3]
+    q2 = math.pi - temp_angle_1
 
-    """
-    Fill in your IK solution here and return the three joint values in q
-    """
+
+    ## modified: delete this if it doesn't work 
+    #temp_val_1 = arm2*math.cos(q2)+arm1
+    #temp_val_2 = arm2*math.sin(q2)
+    #try replacing first x - 0.07 with just x before deleting
+    #y*temp_val_2 + (x-0.07)*temp_val_1
+    #q1 = math.atan2(
+    #    (y*temp_val_1 - (x-0.07)*temp_val_2), 
+    #    (y*temp_val_2 + (x-0.07)*temp_val_1)
+    #)
+    ##
+
+    ###this should work
+    #num = y*(arm2*math.cos(q2)+arm1) - x*(arm2*math.sin(q2))
+    #dnum = (x-0.07)*(arm2*math.cos(q2)+arm1) + y*(arm2*math.sin(q2)) 
+    #q1 = math.atan2(num,dnum)
+    ###
+
+    ## this is another solution delete also if it doesnt work
+    temp_angle_2 = math.asin(arm2*(sin(q2)/r)) - math.asin(y/r)
+    q1 = math.pi*2 + temp_angle_2
+    ##
+    q = [q1, q2, q3]
 
     return q
 
