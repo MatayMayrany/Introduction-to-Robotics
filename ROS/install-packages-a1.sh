d9bc9beec9c0d75a179595dc6dae7f8faa25d962
Matay Mayrany <mayrany@kth.se>
2021-09-05 10:01:22 +0000

    set up scripts for overall ros packages on ubuntu 18.04 and for assignment 1 (ROS)

---
+++ ROS/install-packages-a1.sh
@@ -0,0 +1,9 @@
+# Install ROS packages and such for assignment 1
+
+sudo apt install ros-melodic-ros-tutorials ros-melodic-turtlebot3 ros-melodic-turtlebot3-simulations ros-melodic-navigation libspatialindex-dev libqt4-dev
+
+sudo apt install ros-melodic-rqt ros-melodic-rqt-common-plugins ros-melodic-turtlesim
+
+sudo apt install ros-melodic-turtle-tf2 ros-melodic-tf2-tools ros-melodic-tf
+
+pip install rtree sklearn
